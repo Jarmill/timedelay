73fb0cb9dc3c124c32b24970319fcc65408fe698
Jared Miller <jared.fr.miller@gmail.com>
2021-02-02 15:28:52 -0500

    Initial commit of time delay testing

---
+++ plot_time_delay_discrete_scale.m
@@ -0,0 +1,38 @@
+options = ddeset('AbsTol', 1e-9, 'RelTol', 1e-7, 'Jumps', 0);
+
+T = 1;
+% tau = 0.1;
+% K0 = 1;
+% K1 = 14;
+% sol = dde23(@(t,y,z) -K0*y-K1*z, [tau],@(t) xh0,[0,T], options);
+
+
+tau = 0.3;
+K0 = 1;
+K1 = 5;
+sol = dde23(@(t,y,z) -K0*y-K1*z, [tau],@(t) xh0,[0,T], options);
+
+
+figure(1)
+clf
+hold on
+plot([-tau sol.x], [-1 sol.y])
+plot([-tau, T], [0, 0], ':k')
+xlim([-tau, T])
+hold off
+
+
+% function d = curr_delay(t,y)
+% 
+% d = t - tau;
+% end
+% 
+% function y = history(t)
+%     y = -1;
+% end
+% 
+% function dydt = ddex(t,y,Z)
+% ylag = Z(:,1);
+% % dydt = 0.5*y(1) - ylag(1);
+% dydt = -5*ylag(1);
+% end
